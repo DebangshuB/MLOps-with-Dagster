9c33daf1d3c046e3dff5b36e4fb5d54f7e7e3188
DebangshuB <debangshu133@gmail.com>
2022-05-15 02:26:45 +0530

    download data

---
+++ modules/get_data.py
@@ -0,0 +1,61 @@
+import re
+import yaml
+import requests
+from pandas import DataFrame
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+
+
+def fetch_by_page_no(page_no, data, url):
+    # Get
+    res = requests.get(url.format(page_no))
+
+    # Parse
+    soup = BeautifulSoup(res.content, 'lxml')
+
+    # Find all Property Information Cards
+    data_cards = soup.find_all("li", class_="cardholder")
+
+    # Parse the required data
+    for data_card in data_cards:
+        data.append({
+            "BHK": data_card.select_one('a strong span.val').text.strip(),
+            "SQFT": data_card.select_one('tr.hcol td.size span.val').text.strip(),
+            "Status": data_card.select_one('tr.hcol.w44 td.val').text.strip(),
+            "Age": re.match(
+                ".+(New|Resale).+",
+                data_card.select_one('ul.listing-details').text
+            ).group(1),
+            "Price": data_card.select_one('span.val[itemprop="offers"]').text.strip(),
+            "Unit": data_card.select_one('span.unit').text.strip()
+        })
+
+
+def fetch_data():
+
+    # Opening the global parameters
+    with open("./modules/param.yaml", "r") as stream:
+        try:
+            param = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    url = param["get_data"]["URL"]
+    num_pages = param["get_data"]["NUM_PAGES"]
+
+    page_nos = [i for i in range(num_pages)]
+    data = []
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(
+            fetch_by_page_no,
+            page_nos,
+            [data] * num_pages,
+            [url] * num_pages
+        )
+
+    cols = ["BHK", "Price", "Unit", "SQFT", "Status", "Age"]
+
+    df = DataFrame(data, columns=cols)
+
+    return df
